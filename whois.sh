05077f12eb21ab10f77c5527cd46e9a959e8736c
onuncukoy-dot <68024335+onuncukoy-dot@users.noreply.github.com>
2021-09-26 21:37:08 +0300

    Add files via upload

---
+++ whois.sh
@@ -0,0 +1,23 @@
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+for i in `cat scope` ; do 
+a=$(whois $i  | grep -E 'AT&T Services, Inc.|AT&T')
+echo -e "\n\n ${ORANGE}URL:${NOCOLOR}" ${GREEN}$i ${NOCOLOR} ;echo  "whois:"$a | tee -a test5;done 
+#echo $i $a  | tee test
+
+
+
